360734bc1b048dd1e9a9d8629ea90cae29519080
Borys Cywinski <cyviloo@gmail.com>
2018-03-29 19:52:55 +0200

    Add some concerns about KNN

--- src/engine/knn/KnnAlgorithm.java
+++ src/engine/knn/KnnAlgorithm.java
@@ -30,7 +30,7 @@ public class KnnAlgorithm {
 	}
 
 	/*
-	 * This methode estimates (judges) the label of the given candidate.
+	 * This method estimates (judges) the label of the given candidate.
 	 * 
 	 * It returns an array list of numeric labels
 	 * (which by the way is to contain only one element for a single-labeled data)
@@ -39,21 +39,45 @@ public class KnnAlgorithm {
 	 */
 	public ArrayList<Integer> judge(
 			Sample candidate, int kNeighbours, DistanceMethod method) {
-		System.out.println("AAA");
 		int numberOfLabels = candidate.numberOfLabels();
-		ArrayList<TreeMap<Integer, Double>> scores = new ArrayList<>(numberOfLabels);
+		ArrayList<TreeMap<Integer, Integer>> scores = new ArrayList<>(numberOfLabels);
 		for(int i = 0; i < numberOfLabels; ++i)
 			scores.add(new TreeMap<>());
 
 		// sort array basing on the candidate and the comparator
 		samples.sort(createSampleComparator(candidate, method));
 
+		// promote labels of those k nearest neighbours
 		for(int i = 0; i < kNeighbours; ++i) {
 			Sample neighbour = samples.get(i);
-			
+			for(int labelNo = 0; labelNo < numberOfLabels; ++labelNo) {
+				int neighbourLabel = neighbour.labels.get(labelNo);
+				int newScore;
+				try {
+					newScore = scores.get(labelNo).get(neighbourLabel);
+				} catch(NullPointerException e) {
+					newScore = 0;
+				}
+				scores.get(labelNo).put(neighbourLabel, newScore + 1);
+			}
 		}
 
-		return new ArrayList<Integer>();
+		// find dominating labels
+		ArrayList<Integer> result = new ArrayList<Integer>(numberOfLabels);
+		for(int labelNo = 0; labelNo < numberOfLabels; ++labelNo) {
+			/*
+			 *  for every map:
+			 *  find max value from the values
+			 *  take its key
+			 *  the key means the winning label value for a single label
+			 *  
+			 *  construct the new array list from these values
+			 */
+		}
+
+		// consider equally-sized classes
+
+		return result;
 	}
 
 	private SampleComparator createSampleComparator(Sample model, DistanceMethod method) {
