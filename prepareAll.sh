daaea660b804fae9ea6b6e43c0d0ddfb26f8d33f
Borys Cywinski <cyviloo@gmail.com>
2018-04-23 21:58:42 +0200

    Add xml processing instructions to generation script

--- prepareAll.sh
+++ prepareAll.sh
@@ -3,7 +3,8 @@
 filename="$1"
 xmldir="$2"
 
-echo '<DOCUMENT>' > $filename
+echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' > $filename
+echo '<DOCUMENT>' >> $filename
 ./prepareXML.sh $xmldir >> $filename
 echo '</DOCUMENT>' >> $filename
 
