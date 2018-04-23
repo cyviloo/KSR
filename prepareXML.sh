aa3e9bd0255b41ee32d73c5c48a4b0620ab95442
Borys Cywinski <cyviloo@gmail.com>
2018-04-23 18:31:28 +0200

    Tune prepareXML.sh a little

--- prepareXML.sh
+++ prepareXML.sh
@@ -3,7 +3,7 @@
 sgm_files_directory="$1"
 category="$2" # not implemented yet
 
-function process { egrep '^<REUTERS|^</REUTERS|^<PLACES|<BODY>|^[^<]' "$1" | grep -v '</UNKNOWN>' | sed 's/.*<BODY>/<BODY>/' | sed 's/.*<\/BODY>.*/<\/BODY>/' | sed 's/<D>//g' | sed 's/<\/D>//g' | sed 's/\(<REUTERS\).*\(OLDID.*\)/\1 \2/' | grep -v '^&' | grep -v '^\*' | sed 's/<PLACES><\/PLACES>/<PLACES>x<\/PLACES>/' | sed 's/^.&.*//' | sed 's/&#[0-9][0-9];//g' ; }
+function process { egrep '^<REUTERS|^</REUTERS|^<PLACES|<BODY>|^[^<]' "$1" | grep -v '</UNKNOWN>' | sed 's/.*<BODY>/<BODY>/' | sed 's/.*<\/BODY>.*/<\/BODY>/' | sed 's/<D>//g' | sed 's/<\/D>//g' | sed 's/\(<REUTERS\).*\(OLDID.*\)/\1 \2/' | grep -v '^&' | grep -v '^\*' | sed 's/<PLACES><\/PLACES>/<PLACES>x<\/PLACES>/' | sed 's/^.&.*//' | sed -r 's/\&#[0-9]+\;//g' ; }
 
 sgm_files=(`ls ${sgm_files_directory}*.sgm`)
 
