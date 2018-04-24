4902aec25c02256f4d4d51dbe364cff0a77f219a
Borys Cywinski <cyviloo@gmail.com>
2018-04-24 22:28:02 +0200

    Fix XML generation scripts. Add generated XMLs.

--- prepareXML.sh
+++ prepareXML.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 sgm_files_directory="$1"
-category="$2" # not implemented yet
+data_dir="data"
 
 function process { egrep '^<REUTERS|^</REUTERS|^<PLACES|^<TOPIC|<BODY>|^[^<]' "$1" | grep -v '</UNKNOWN>' | sed 's/.*<BODY>/<BODY>/' | sed 's/.*<\/BODY>.*/<\/BODY>/' | sed 's/<D>//g' | sed 's/<\/D>//g' | sed 's/\(<REUTERS\).*\(OLDID.*\)/\1 \2/' | grep -v '^&' | grep -v '^\*' | sed 's/<PLACES><\/PLACES>/<PLACES>x<\/PLACES>/' | sed 's/<TOPICS><\/TOPICS>/<TOPICS>x<\/TOPICS>/' | sed 's/^.&.*//' | sed -r 's/\&#[0-9]+\;//g' ; }
 
 sgm_files=(`ls ${sgm_files_directory}*.sgm`)
+counter=1
 
 for FILE in ${sgm_files[@]}; do
-	process "$FILE"
+	filename="$data_dir/reuters${counter}.xml"
+	counter=$(($counter+1))
+	echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' > $filename
+	echo '<DOCUMENT>' >> $filename
+	process "$FILE" >> $filename
+	echo '</DOCUMENT>' >> $filename
 done
 
