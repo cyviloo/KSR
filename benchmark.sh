d0a429524a3e7ea66202366d67435b89cd374245
Borys Cywinski <cyviloo@gmail.com>
2018-04-26 09:47:00 +0200

    Improve benchmark.sh a little

--- benchmark.sh
+++ benchmark.sh
@@ -15,7 +15,24 @@ kNumber=(3 7 11)
 numbers_cleaning="true"
 
 bindir=bin
+freshFile="experiment.log"
 runCmd="java -cp $bindir app.Main"
 
-$runCmd ${xmlDataFiles[0]} "null" ${etiquettes[0]} $trainingPctSize ${similarity[0]} ${nNumber[0]} ${minAcceptableSimilarity[0]} ${stemming[1]} ${metric[0]} ${kNumber[0]} $numbers_cleaning
 
+for xmlFile in ${xmlDataFiles[@]}; do
+  for list in ${stoplist[@]}; do
+#   for etiq in ${etiquettes[@]}; do
+#      for sim in ${similarity[@]}; do
+        for minSim in ${minAcceptableSimilarity[@]}; do
+          for stem in ${stemming[@]}; do
+            for mtr in ${metric[@]}; do
+              for k in ${kNumber[@]}; do
+                $runCmd $xmlFile $list ${etiquettes[0]} $trainingPctSize binary ${nNumber[1]} $minSim $stem $mtr $k $numbers_cleaning && mv $freshFile "test_${xmlFile}_${stopList}_${etiquettes[0]}_${trainingPctSize}_binary_${nNumber[1]}_${minSim}_${stem}_${mtr}_${k}_${numbers_cleaning}.log"
+              done
+            done
+          done
+        done
+#      done
+#   done
+  done
+done
